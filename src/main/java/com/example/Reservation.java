47641bae40dabd99ab45eda054da00525d71d34e
pedro-2493 <pedrozamora1993@gmail.com>
2026-02-16 19:42:01 -0500

    Creación de la clase Reservation y los atributos de instancia

---
+++ src/main/java/com/example/Reservation.java
@@ -0,0 +1,17 @@
+package com.example;
+/**
+ * Clase que representa una reserva de hotel
+ * Gestiona la información de las reservas incluyendo datos del huésped,
+ * fechas de salida y tipo de habitacón 
+ * 
+ * @author Pedro Luis Zamora Martinez
+ * @version 1.0
+ */
+public class Reservation {
+private String reservationId;
+private String guestName;
+private String roomType;
+private int numberOfNights;
+private double pricePerNight;
+
+}
