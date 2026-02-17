dfb4b061f464c1d608ec56a7647395be189d196b
pedro-2493 <pedrozamora1993@gmail.com>
2026-02-16 20:00:13 -0500

    Creacion de los contructores por defecto y parametrizados

--- src/main/java/com/example/Reservation.java
+++ src/main/java/com/example/Reservation.java
@@ -14,4 +14,35 @@ private String roomType;
 private int numberOfNights;
 private double pricePerNight;
 
+/**
+ * Constructor por defecto
+ * Inicializa todos los atributos con valores predeterminados
+ */
+public Reservation(){
+    this.reservationId = null;
+    this.guestName = null;
+    this.roomType = null;
+    this.numberOfNights = 0;
+    this.pricePerNight = 0.0;
+}
+
+/**
+ * Constructor parametrizado
+ * permite crear una reserva con todos los datos iniciales
+ * 
+ * @param reservationId Código unico de la reserva 
+ * @param guestName Nombre completo del huesped
+ * @param roomType Tipo de habitación (Sencilla, Doble, suite)
+ * @param numberOfNights Número de noches de estadia
+ * @param pricePerNight Precio por noche en dolares
+ */
+public Reservation(String reservationId, String guestName, String roomType, int numberOfNights, double pricePerNight){
+    this.reservationId = reservationId;
+    this.guestName = guestName;
+    this.roomType = roomType;
+    this.numberOfNights = numberOfNights;
+    this.pricePerNight = pricePerNight;
+}
+
+
 }
