7f308fba53c74cdc9e25c6792a12dbdfdab6855b
pedro-2493 <pedrozamora1993@gmail.com>
2026-02-16 21:09:29 -0500

    Creacion de la clase ReservationDemo para demostrar funcionalidad del sistema de reservas creando reserva 1 con constructores por defecto y reserva 2 con constructores parametrizados

---
+++ src/main/java/com/example/ReservationDemo.java
@@ -0,0 +1,47 @@
+package com.example;
+
+/**
+ * Clase de demostración para el sistema de reservas de hotel
+ * Muestra el uso de constructores, guetters, setters y validaciones
+ * 
+ * @author Pedro Luis Zamora Martinez
+ * @version 1.0
+ */
+public class ReservationDemo {
+    public static void main(String[] args) {
+        System.out.println("=========---SISTEMA DE RESERVAS - HOTEL PARADISE");
+
+        //Reserva 1: Constructor por defecto.
+        System.out.println(">>> Creando Reserva 1 con constructor por defecto.\n");
+        Reservation reserva1 = new Reservation();
+
+        System.out.println("Estado inicial:");
+        System.out.println(reserva1);
+
+        // Modificar usando setters.
+        System.out.println("\n>>> Configurando reserva con setters.");
+        reserva1.setReservationId("R001");
+        reserva1.setGuestName("Maria Osorio");
+        reserva1.setRoomType("suite ejecutiva");
+        reserva1.setNumberOfNights(5);
+        reserva1.setPricePerNigth(180.00);
+
+        System.out.println("\nReserva 1 completa:");
+        System.out.println(reserva1);
+
+        // Reserva 2: Constructor parametrizado.
+        System.out.println("\n\n>>> Creando Reserva con constructor parametrizado.\n");
+        Reservation reserva2 = new Reservation(
+                "R002",
+                "Carolina Ramirez",
+                "Gabitacion Doble",
+                5,
+                95.50
+        );
+
+        System.out.println("Reserva 2 creada:");
+        System.out.println(reserva2);
+
+        
+    }
+}
